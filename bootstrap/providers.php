b4b03d3e98c0ab63ab91c069b1a40e490d52aa02
innocentez <kozlov.developer@gmail.com>
2024-10-25 22:55:21 +0300

    Backend/Core: Providers update

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\BladeComponentsServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
 
     // Modules
     Modules\Lead\Providers\LeadModuleServiceProvider::class,
