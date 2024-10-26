faf734f43c29fbea51db0d6bb13a55d41d9b148b
innocentez <kozlov.developer@gmail.com>
2024-10-26 10:14:18 +0300

    Backend/Lead: routes update

--- modules/Lead/src/Http/routes.php
+++ modules/Lead/src/Http/routes.php
@@ -1,1 +1,9 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+use Modules\Lead\Http\Controllers\LeadController;
+
+Route::group(['prefix' => 'lead', 'as' => 'lead'], function ()
+{
+    Route::post('create', [LeadController::class, 'store'])->name('.store');
+});
